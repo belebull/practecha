6451140546b31cd259bfb4d7eb297f7e0d89049d
Beleicia Bullock <beleiciabullock@Beleicias-MacBook-Pro-2.local>
2021-02-08 23:59:47 -0600

    lofi: added hero section without image

---
+++ components/PrimaryBtn.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <a
+      :href="href"
+      class="block bg-gray-300 border-gray-500 h-16 w-48 rounded-md font-body text-lg md:text-xl text-center align-middle py-4"
+      >{{ buttonText }}</a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: { type: String, default: 'Button' },
+    href: { type: String, default: 'https://www.practecha.com' },
+  },
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="container mx-auto py-16 px-8 sm:px-auto"><menu-bar /></div>
+  <div class="container mx-auto py-16 px-8 sm:px-auto">
+    <menu-bar />
+    <!---Hero Section--->
+    <div class="mt-8 flex flex-col items-center md:items-start md:w-3/5">
+      <div class="font-header text-3xl text-center md:text-left my-5">
+        We're making technical interviewing work for women and companies.
+      </div>
+      <div class="text-center md:text-left font-body text-lg mb-5">
+        We help women build their technical interviewing skills through mock
+        interviews with engineers and companies looking to improve their
+        interviewing process.
+      </div>
+      <div class="">
+        <primary-btn button-text="Join Waitlist"></primary-btn>
+      </div>
+    </div>
+
+    <!--About Section-->
+  </div>
 </template>
 
 <script>
 import MenuBar from '~/components/MenuBar.vue'
+import PrimaryBtn from '~/components/PrimaryBtn.vue'
 export default {
-  components: { MenuBar },
+  components: { MenuBar, PrimaryBtn },
 }
 </script>
 
