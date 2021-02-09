22a7d910cd80e27a225001dc58a6ce593d9c1986
Beleicia Bullock <beleiciabullock@Beleicias-MacBook-Pro-2.local>
2021-02-08 23:33:35 -0600

    lofi: added menu

---
+++ components/MenuBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <div class="flex flex-row justify-between items-center">
+      <div class="logo">
+        <h1 class="font-header font-bold text-3xl md:text-5xl">Practecha</h1>
+      </div>
+      <div class="hidden md:block">
+        <a
+          href=""
+          class="block bg-gray-300 border-gray-500 h-16 w-48 rounded-md font-body text-lg md:text-xl text-center align-middle py-4"
+          >Join Waitlist</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style></style>
