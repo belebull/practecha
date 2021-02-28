8a372117c38d91ad0f67f85b2bbeab8be69a119f
Beleicia Bullock <beleiciabullock@Beleicias-MacBook-Pro-2.local>
2021-02-28 16:27:58 -0600

    changed title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'practecha',
+    title: "Practecha - Let's build a better technical interviewing process.",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
